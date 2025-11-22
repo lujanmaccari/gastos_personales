1b6acc05aa8c1fd46322bce701cc783993226b18
Lujan Maccari <lmaccari@pentass.com>
2025-11-22 20:51:47 -0300

    se agrega listado de base para CategoriaListView

--- master/apps/categoria/urls.py
+++ master/apps/categoria/urls.py
@@ -1,8 +1,8 @@
 
 from django.urls import path
-from .views import CategoriaView, CategoriaCreateView
+from .views import CategoriaListView, CategoriaCreateView
 
 urlpatterns = [
-    path('categoria/', CategoriaView.as_view(), name='categoria'),
+    path('categoria/', CategoriaListView.as_view(), name='categoria'),
     path('categoria/create/', CategoriaCreateView.as_view(), name='categoria_create'),
 ]
--- master/apps/categoria/views.py
+++ master/apps/categoria/views.py
@@ -1,26 +1,57 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView, View,CreateView
-from .models import Categoria
+from .models import Categoria, Color, Icono
 from django.urls import reverse_lazy
+from apps.utils.calculations import asignar_iconos_y_colores_categorias_gastos
 
-# Create your views here.
-class CategoriaView(LoginRequiredMixin, TemplateView):
+class UserCategoriaQuerysetMixin:
+    """Filtra las categorías para que cada usuario solo vea las suyas."""
+    def get_queryset(self):
+        return Categoria.objects.filter(usuario=self.request.user)
+
+
+class CategoriaListView(LoginRequiredMixin,UserCategoriaQuerysetMixin, TemplateView):
+    model = Categoria
     template_name = 'categoria/categoria.html'
-
+    context_object_name = 'categoria'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categorias = self.get_queryset()
+        
+        context['categorias'] = categorias
+        return context
+    
 
 class CategoriaFieldsMixin:
     fields = ['nombre','descripcion', 'color', 'icono']
 
 
-class CategoriaCreateView(LoginRequiredMixin,CategoriaFieldsMixin, CreateView):
+class CategoriaCreateView(LoginRequiredMixin, CategoriaFieldsMixin, CreateView):
     model = Categoria
     template_name = 'categoria/categoria_form.html'
+    fields = ['nombre', 'color', 'icono']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # traer listados generales desde BD
+        iconos_qs = Icono.objects.all()
+        colores_qs = Color.objects.all()
+
+        # convertir a formato esperado por la función
+        iconos_list = [{"nombre": i.icono} for i in iconos_qs]
+        iconos_validos = asignar_iconos_y_colores_categorias_gastos(iconos_list)
+
+        context['iconos'] = iconos_validos
+        context['colores'] = colores_qs
+
+        return context
 
     def form_valid(self, form):
         form.instance.usuario = self.request.user
         return super().form_valid(form)
-    
+
     def get_success_url(self):
         return reverse_lazy('categoria')
-    
