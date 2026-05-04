ff032770752a95a1d574ef6168cb6a364e75aa7e
Lujan Maccari <lmaccari@pentass.com>
2026-05-04 20:46:34 -0300

    fix deploy configuration

--- master/apps/simulador/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import SimuladorView, SimuladorCalculoView
-
-urlpatterns = [
-    path("simulador/", SimuladorView.as_view(), name="simulador"),
-    path("calcular/", SimuladorCalculoView.as_view(), name="simulador-calcular"),
-]
--- master/apps/usuario/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib.auth.views import LoginView
-from .views import RegisterView, LogoutView 
-
-urlpatterns = [
-    path("login/", LoginView.as_view(template_name="usuario/login.html"), name="login"),
-    path("register/", RegisterView.as_view(), name="register"),
-    path("logout/", LogoutView.as_view(), name="logout"),  
-]
